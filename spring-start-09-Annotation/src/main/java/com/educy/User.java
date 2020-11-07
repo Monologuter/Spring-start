f074fa635f8ea790d60ae7dc5c7b287c0a636541
cyy <2439644676@qq.com>
2020-11-07 09:10:48 +0800

    Component的衍生注解

--- spring-start-09-Annotation/src/main/java/com/educy/User.java
+++ spring-start-09-Annotation/src/main/java/com/educy/User.java
@@ -2,6 +2,9 @@ package com.educy;
 
 import lombok.Data;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
 
@@ -12,6 +15,9 @@ import java.io.Serializable;
  * @Description:
  */
 
+
+//Component的衍生注解  @Repository @Service @Controller 只是作用的不同的地方  起到的作用是完全一样的 只是起到一个标识的作用
+//但是在spring和Mybatis整合的时候不能使用@Repository 和 @Component
 
 @Component("u")
 @Data
